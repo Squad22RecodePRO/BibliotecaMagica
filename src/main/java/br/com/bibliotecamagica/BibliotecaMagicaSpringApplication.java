ef3242c61555a11ee23f69cd6af273356211da3d
Squad22RecodePRO <108561600+Squad22RecodePRO@users.noreply.github.com>
2022-11-14 13:20:30 -0300

    adm usuario

---
+++ src/main/java/br/com/bibliotecamagica/BibliotecaMagicaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.bibliotecamagica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaMagicaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaMagicaSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/bibliotecamagica/controller/UsuarioController.java
@@ -0,0 +1,77 @@
+package br.com.bibliotecamagica.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.bibliotecamagica.model.Usuario;
+import br.com.bibliotecamagica.repository.UsuarioRepository;
+
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("usuario/home.html");
+		
+		List<Usuario> usuario = usuarioRepository.findAll();
+		modelAndView.addObject("usuario", usuario);
+		
+		return modelAndView;
+	}
+	
+	@GetMapping("/cadastrar")
+    public ModelAndView cadastrar() {
+        ModelAndView modelAndView = new ModelAndView("usuario/cadastro");
+
+        modelAndView.addObject("usuario", new Usuario());
+       
+        return modelAndView;
+    }
+    
+	@PostMapping("/cadastrar")
+	public ModelAndView cadastrar(Usuario usuario) throws IOException {
+
+		ModelAndView modelAndView = new ModelAndView("redirect:/usuario");
+
+		usuarioRepository.save(usuario);
+
+		return modelAndView;
+	}
+
+    @GetMapping("/{id}/editar")
+    public ModelAndView editar(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("usuario/editar");
+
+        modelAndView.addObject("usuario", usuarioRepository.getOne(id));
+        
+        return modelAndView;
+    }
+
+    @PostMapping("/{id}/editar")
+    public String salvar(Usuario usuario) {
+    
+        usuarioRepository.save(usuario);
+
+        return "redirect:/usuario";
+    }
+
+    @GetMapping("/{id}/excluir")
+    public String excluir(@PathVariable Long id) {
+       usuarioRepository.deleteById(id);
+
+        return "redirect:/usuario";
+    }
+
+}
